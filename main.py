2c8b637c8d735837af5d6329a35487f8916aba46
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-05-14 13:18:24 +0100

    [FEAT] Waitress Server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from inventoryutils import *
+from waitress import serve
 import sqlite3
 
 app = Flask(__name__)
@@ -68,4 +69,4 @@ def orders():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=5000)
